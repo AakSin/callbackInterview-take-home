a92bf1ed35c9e0f84b2c5f11d22b8801c976c3fb
dibeling <ibelingdaniel@gmail.com>
2024-03-05 16:57:12 -0600

    Next.js, public/, constants

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+// import { fetchEthereumAddressNfts } from '@/lib/fetchEthereumAddressNfts';
+// import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>Hello!</p>
+            </header>
+        </div>
+    );
+}
+
+export default App;
