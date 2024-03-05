a57934dd449388c40ef1b89bf2345deffa81233d
dibeling <ibelingdaniel@gmail.com>
2024-03-05 17:28:05 -0600

    fix absolute import

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 // import { fetchEthereumAddressNfts } from '@/lib/fetchEthereumAddressNfts';
-// import logo from './logo.svg';
-// import './App.css';
 
 function App() {
     return (
